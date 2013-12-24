fd55ff40c994cabb838525cb46d6e12190b873fc
Keith Brinks <keithbrinks@me.com>
2013-12-24 10:23:47 -0500

    Remove Woopra tracking code

--- header.php
+++ header.php
@@ -11,22 +11,8 @@
 	<![endif]-->
 	<link href="http://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel="stylesheet" type="text/css">
 	
-	<?php if (!is_user_logged_in()) { ?>
-		<!-- Start of Woopra Code -->
-		<script>
-		(function(){
-		    var t,i,e,n=window,o=document,a=arguments,s="script",r=["config","track","identify","visit","push","call"],c=function(){var t,i=this;for(i._e=[],t=0;r.length>t;t++)(function(t){i[t]=function(){return i._e.push([t].concat(Array.prototype.slice.call(arguments,0))),i}})(r[t])};for(n._w=n._w||{},t=0;a.length>t;t++)n._w[a[t]]=n[a[t]]=n[a[t]]||new c;i=o.createElement(s),i.async=1,i.src="//static.woopra.com/js/w.js",e=o.getElementsByTagName(s)[0],e.parentNode.insertBefore(i,e)
-		})("woopra");
-		
-		woopra.config({
-		    domain: 'keithbrinks.com'
-		});
-		woopra.track();
-		</script>
-		<!-- End of Woopra Code -->	
-	<?php } ?>
-	
 	<?php wp_head(); ?>
+	
 </head>
 <body <?php body_class(); ?>>
 
