0659b3e2362a9001488765dcc8ca3b75229e8f5b
Keith Brinks <keithbrinks@me.com>
2014-03-05 16:47:40 -0500

    Add support for pages

--- page.php
+++ page.php
@@ -1,27 +1,23 @@
 <?php get_header(); ?>
 
-	<div id="contaner">
-
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-				
-			<div class="page" id="post-<?php the_ID(); ?>">
-
-				<h1><?php the_title(); ?></h1>
-
-				<div class="entry">
-
-					<?php the_content(); ?>
-
-					<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
-
-				</div> <!-- /entry -->
-
-			</div> <!-- /page -->
-
-		<?php endwhile; endif; ?>
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			
+		<article id="page" id="post-<?php the_ID(); ?>">
 		
-	</div> <!-- /container -->
+			<header id="page_header">
 
-	<?php get_sidebar(); ?>
+				<h2><?php the_title(); ?></h2>
+				
+			</header> <!-- /page_header -->
+
+			<div id="page_content" class="entry">
+
+				<?php the_content(); ?>
+
+			</div> <!-- /post_content -->
+
+		</article> <!-- /page -->
+
+	<?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
