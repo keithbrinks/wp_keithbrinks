18e88432da8d73ab82df9ed8ed18010bc79c48e9
Keith Brinks <keithbrinks@me.com>
2014-03-05 17:31:57 -0500

    Refactor 404 page

--- 404.php
+++ 404.php
@@ -1,20 +1,20 @@
 <?php get_header(); ?>
-
-	<div id="right_column">
-
-		<main id="kb_main_area">
-
-			<h2 class="page_title">404 - Page Not Found</h2>
 			
-			<div class="post_content">
-				
-				<p>This was my chance to do something creative. But I didn't. Instead you get this rather boring page that does nothing but tell you that the page you requested doesn't exist.</p>
-				<p>Good luck finding what you were looking for.</p>
+	<article id="page" class="404">
 	
-			</div>
-	
-		</main> <!-- /kb_main_area -->
-		
-	</div><!-- /right_column -->
+		<header id="page_header">
+
+			<h2>404 - Page Not Found</h2>
+			
+		</header> <!-- /page_header -->
+
+		<div id="page_content" class="entry">
+
+			<p>This was my chance to do something creative. But I didn't. Instead you get this rather boring page that does nothing but tell you that the page you requested doesn't exist.</p>
+			<p>Good luck finding what you were looking for.</p>
+
+		</div> <!-- /post_content -->
+
+	</article> <!-- /page -->
 
 <?php get_footer(); ?>
